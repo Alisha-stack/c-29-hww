6cfd311ad25ade9190794fb6c18ce107cbe5eed7
Alisha-stack <74552886+Alisha-stack@users.noreply.github.com>
2021-01-13 19:40:54 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,23 @@
+class Block {
+    constructor(x,y,width,height) 
+    {
+      var options = 
+      {
+          isStatic: false,
+          density : 0.0001
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display()
+    {
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("red");
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+  }
+  
